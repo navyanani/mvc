17b78f8491440e59018be3121474e90374fbb6fc
navya <navyavoosakoyala@gmail.com>
2016-09-19 14:35:27 +0530

    view

---
+++ view/view_class.php
@@ -0,0 +1,18 @@
+<?php
+class ViewClass{
+	public $arr = array();
+	
+	function show()
+	{
+		extract($this->arr);
+		include_once "view/".$_GET["control"]."View.html";
+	}
+	function assign($name , $value){
+		$this->arr[$name] = $value;
+	}
+	public function __destruct(){
+	
+		$this ->show();
+	}
+	}
+?>
